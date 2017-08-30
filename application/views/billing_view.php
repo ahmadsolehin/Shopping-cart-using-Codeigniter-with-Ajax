80925b0a88d5a99b952d99a7f3138c78455a3607
-Mayonis masin- <solehinjr@gmail.com>
2017-08-30 17:11:33 +0800

    Update billing_view.php

--- application/views/billing_view.php
+++ application/views/billing_view.php
@@ -27,7 +27,7 @@
             <div align="center">
                 <h1 align="center">Billing Info</h1>
                 <table border="0" cellpadding="2px">
-                    <tr><td>Order Total:</td><td><strong>INR <?php echo $grand_total; ?></strong></td></tr>
+                    <tr><td>Order Total:</td><td><strong>RM <?php echo $grand_total; ?></strong></td></tr>
                     <tr><td>Your Name:</td><td><input type="text" name="name" required=""/></td></tr>
                     <tr><td>Address:</td><td><input type="text" name="address" required="" /></td></tr>
                     <tr><td>Email:</td><td><input type="text" name="email" required="" /></td></tr>
